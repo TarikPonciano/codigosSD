ae2f9aa6a397d87e9841ad57277b083e59625e27
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2020-04-22 03:21:44 -0300

    Update Calculadora.java

--- JavaClientSocket_Calculadora/src/Calculadora.java
+++ JavaClientSocket_Calculadora/src/Calculadora.java
@@ -7,27 +7,27 @@ public class Calculadora {
 		switch (op) {
 
     case 1:  
-		chamadas++;
+		
 		System.out.println("Metodo soma chamado \n");
-		System.out.println("Total de chamadas: "+chamadas);
+		
 		return a + b;
     
     case 2:  
-		chamadas++;
+		
 		System.out.println("Metodo subtracao chamado \n");
-		System.out.println("Total de chamadas: "+chamadas);
+		
 		return a - b;
                      
     case 3:  
-		chamadas++;
+		
 		System.out.println("Metodo multiplicacao chamado \n");
-		System.out.println("Total de chamadas: "+chamadas);
+		
 		return a * b;
                      
     case 4:  
-		chamadas++;
+		
 		System.out.println("Metodo divisao chamado \n");
-		System.out.println("Total de chamadas: "+chamadas);
+		
 		return a / b;
                      
 	  default: System.out.println("Operacao invalida");
