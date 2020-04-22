9ad8edd32c7b232410339678db68943e484563e1
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2020-04-22 03:24:20 -0300

    Update Calculadora.java

--- JavaServerSocket_Calculadora/Calculadora.java
+++ JavaServerSocket_Calculadora/Calculadora.java
@@ -2,7 +2,36 @@ public class Calculadora {
     public String sayHello(String nome, String sobrenome) {
         return "Fala "+ nome + " " + sobrenome;
     }
-    public double soma(double oper1, double oper2) {
-        return oper1 + oper2;
-    }
+   public double calcular(double a, double b, int op) throws RemoteException {
+		switch (op) {
+
+    case 1:  
+		
+		System.out.println("Metodo soma chamado \n");
+		
+		return a + b;
+    
+    case 2:  
+		
+		System.out.println("Metodo subtracao chamado \n");
+		
+		return a - b;
+                     
+    case 3:  
+		
+		System.out.println("Metodo multiplicacao chamado \n");
+		
+		return a * b;
+                     
+    case 4:  
+		
+		System.out.println("Metodo divisao chamado \n");
+		
+		return a / b;
+                     
+	  default: System.out.println("Operacao invalida");
+    return "Operacao invalida";
+		     
+  	}
+  }
 }
