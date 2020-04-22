27ba854617e40b30efd1824a6f7fd844f943e7ec
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2020-04-22 02:07:17 -0300

    Update CalculadoraCliente.java

--- ExemploRMI/src/CalculadoraCliente.java
+++ ExemploRMI/src/CalculadoraCliente.java
@@ -2,7 +2,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-
+import java.util.Scanner;
 
 public class CalculadoraCliente {
 	
