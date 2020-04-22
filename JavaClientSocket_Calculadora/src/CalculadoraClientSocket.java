7d642ed2fe7fd4c32bf44fe1c5908c934e2cf3c8
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2020-04-22 03:23:26 -0300

    Update CalculadoraClientSocket.java

--- JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
+++ JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
@@ -10,49 +10,50 @@ public class CalculadoraClientSocket {
   public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-  Scanner sc = new Scanner(System.in);	
+	  Scanner sc = new Scanner(System.in);	
 
-  System.out.println("Insira a operacao:\n");
+	  System.out.println("Insira a operacao:\n");
 
-  System.out.println("1. Soma \n 2. Subtracao \n 3. Multiplicacao \n 4. Divisao \n");
+	  System.out.println("1. Soma \n 2. Subtracao \n 3. Multiplicacao \n 4. Divisao \n");
 
-  int op = sc.nextInt();
-			
-  while (op<1 || op>4) {
-	System.out.println("Operacao invalida, insira novamente: \n");
-	op = sc.nextInt();
-  }
-			
-  System.out.println("Insira o primeiro inteiro:\n");
-  double a = sc.nextDouble();
+	  int op = sc.nextInt();
 
-  System.out.println("Insira o segundo inteiro:\n");
-  double b = sc.nextDouble();
+	  while (op<1 || op>4) {
+		System.out.println("Operacao invalida, insira novamente: \n");
+		op = sc.nextInt();
+	  }
 
-  String result="";
+	  System.out.println("Insira o primeiro inteiro:\n");
+	  double a = sc.nextDouble();
 
-  try {
+	  System.out.println("Insira o segundo inteiro:\n");
+	  double b = sc.nextDouble();
 
-        	//Conex�o com o Servidor
-            Socket clientSocket = new Socket("192.168.0.11", 9090); // Endereço do ip do servidor a se conectar e a porta de conexão
-            DataOutputStream socketSaidaServer = new DataOutputStream(clientSocket.getOutputStream()); // Criação de canal de conexão para envio de dados ao servidor
-            
-            //Enviando os dados
-            socketSaidaServer.writeBytes(op+"\n"); // Adição de informação em forma de Bytes em uma mensagem para enviar ao Servidor
-            socketSaidaServer.writeBytes(a+ "\n"); // Adição de informação em forma de Bytes em uma mensagem para enviar ao Servidor
-            socketSaidaServer.writeBytes(b+ "\n");// Adição de informação em forma de Bytes em uma mensagem para enviar ao Servidor
-            socketSaidaServer.flush(); // Envio da mensagem
+	  String result="";
 
-            //Recebendo a resposta
-            BufferedReader messageFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Canal para recebimento de dados enviados pelo Servidor 
-            result=messageFromServer.readLine(); // Mensagem recebida
-            
-            System.out.println("resultado="+result); // Impressão da mensagem recebida
-            clientSocket.close(); // Encerramento da conexão com o Servidor
+	  try {
 
-   } catch (IOException e) {
-            e.printStackTrace();
-        }
+			//Conex�o com o Servidor
+		    Socket clientSocket = new Socket("192.168.0.11", 9090); // Endereço do ip do servidor a se conectar e a porta de conexão
+		    DataOutputStream socketSaidaServer = new DataOutputStream(clientSocket.getOutputStream()); // Criação de canal de conexão para envio de dados ao servidor
+
+		    //Enviando os dados
+		    socketSaidaServer.writeBytes(op+"\n"); // Adição de informação em forma de Bytes em uma mensagem para enviar ao Servidor
+		    socketSaidaServer.writeBytes(a+ "\n"); // Adição de informação em forma de Bytes em uma mensagem para enviar ao Servidor
+		    socketSaidaServer.writeBytes(b+ "\n");// Adição de informação em forma de Bytes em uma mensagem para enviar ao Servidor
+		    socketSaidaServer.flush(); // Envio da mensagem
+
+		    //Recebendo a resposta
+		    BufferedReader messageFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Canal para recebimento de dados enviados pelo Servidor 
+		    result=messageFromServer.readLine(); // Mensagem recebida
+
+		    System.out.println("resultado="+result); // Impressão da mensagem recebida
+		    clientSocket.close(); // Encerramento da conexão com o Servidor
+
+	   } 
+	     catch (IOException e) {
+		    e.printStackTrace();
+	     }
 
 
 }
