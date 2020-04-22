296eea448674bbe30ef6c26f226fce0277a58134
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2020-04-22 03:15:38 -0300

    Update CalculadoraClientSocket.java

--- JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
+++ JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
@@ -3,25 +3,44 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class CalculadoraClientSocket {
 
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-		double oper1=10,oper2=20;
-		int operacao=1; //1-somar 2-subtrair 3-dividir 4-multiplicar
-		String result="";
+  Scanner sc = new Scanner(System.in);	
+
+  System.out.println("Insira a operacao:\n");
+
+	System.out.println("1. Soma \n 2. Subtracao \n 3. Multiplicacao \n 4. Divisao \n");
+
+	int op = sc.nextInt();
+			
+	while (op<1 || op>4) {
+		System.out.println("Metodo invalido, insira novamente: \n");
+		op = sc.nextInt();
+	}
+			
+	System.out.println("Insira o primeiro inteiro:\n");
+	double a = sc.nextDouble();
+
+	System.out.println("Insira o segundo inteiro:\n");
+	double b = sc.nextDouble();
+
+	String result="";
+
         try {
 
-        	//Conexão com o Servidor
+        	//Conexï¿½o com o Servidor
             Socket clientSocket = new Socket("192.168.0.11", 9090);
             DataOutputStream socketSaidaServer = new DataOutputStream(clientSocket.getOutputStream());
             
             //Enviando os dados
-            socketSaidaServer.writeBytes(operacao+"\n");
-            socketSaidaServer.writeBytes(oper1+ "\n");
-            socketSaidaServer.writeBytes( oper2+ "\n");
+            socketSaidaServer.writeBytes(op+"\n");
+            socketSaidaServer.writeBytes(a+ "\n");
+            socketSaidaServer.writeBytes(b+ "\n");
             socketSaidaServer.flush();
 
             //Recebendo a resposta
@@ -38,5 +57,3 @@ public class CalculadoraClientSocket {
 
 
 	}
-
-}
