ee90af30d0cd3636bccc52a3216852f7309fb964
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2020-04-22 03:23:04 -0300

    Update CalculadoraClientSocket.java

--- JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
+++ JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class CalculadoraClientSocket {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
   Scanner sc = new Scanner(System.in);	
