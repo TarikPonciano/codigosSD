48ececb363c426ab815ace0f4ce84478c7a31495
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2020-04-22 02:14:36 -0300

    Update CalculadoraCliente.java

--- ExemploRMI/src/CalculadoraCliente.java
+++ ExemploRMI/src/CalculadoraCliente.java
@@ -7,12 +7,23 @@ import java.util.Scanner;
 public class CalculadoraCliente {
 	
 	public static void main(String[] args) {
+		
 		Registry reg = null;
 		ICalculadora calc;		
 		try {
 			reg = LocateRegistry.getRegistry(1099);
 			calc = (ICalculadora) reg.lookup("calculadora");
-			System.out.println(calc.soma(3,2));
+			
+			System.out.println("Insira a operação:\n");
+			System.out.println("1. Soma \n 2. Subtracao \n 3. Multiplicacao \n 4. Divisao \n");
+			int op = sc.nextInt();
+			System.out.println("Insira o primeiro inteiro:\n");
+			int op1 = sc.nextInt();
+			System.out.println("Insira o segundo inteiro:\n");
+			int op2 = sc.nextInt();
+			
+			System.out.println(calc.calcular(op1,op2,op));
+			
 		} catch (RemoteException | NotBoundException e) {
 				System.out.println(e);
 				System.exit(0);
@@ -20,3 +31,4 @@ public class CalculadoraCliente {
 	}		
 
 }
+
