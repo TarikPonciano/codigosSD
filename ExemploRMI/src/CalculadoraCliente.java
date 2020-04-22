f2286d42989b24dde03975d0826905105056e28f
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2020-04-22 02:21:22 -0300

    Update CalculadoraCliente.java

--- ExemploRMI/src/CalculadoraCliente.java
+++ ExemploRMI/src/CalculadoraCliente.java
@@ -17,6 +17,12 @@ public class CalculadoraCliente {
 			System.out.println("Insira a operação:\n");
 			System.out.println("1. Soma \n 2. Subtracao \n 3. Multiplicacao \n 4. Divisao \n");
 			int op = sc.nextInt();
+			
+			while (op<1 || op>4) {
+				System.out.println("Metodo invalido, insira novamente: \n");
+				op = sc.nextInt();
+			}
+			
 			System.out.println("Insira o primeiro inteiro:\n");
 			int op1 = sc.nextInt();
 			System.out.println("Insira o segundo inteiro:\n");
